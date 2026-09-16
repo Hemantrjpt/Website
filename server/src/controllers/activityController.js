3ea8d073c512b48a81c03a057d6d0d00aacd53c6
Hemantrjpt <137674202+Hemantrjpt@users.noreply.github.com>
2026-09-16 21:25:28 +0530

    Delete server directory

--- server/src/controllers/activityController.js
+++
@@ -1,9 +0,0 @@
-const { readJson } = require('../db/store');
-
-// GET /api/admin/activity
-async function getActivity(req, res) {
-  const log = await readJson('activity.json');
-  res.json(log);
-}
-
-module.exports = { getActivity };
--- server/src/routes/activity.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { requireAdmin } = require('../middleware/auth');
-const ctrl = require('../controllers/activityController');
-
-const router = express.Router();
-
-router.get('/admin/activity', requireAdmin, ctrl.getActivity);
-
-module.exports = router;
